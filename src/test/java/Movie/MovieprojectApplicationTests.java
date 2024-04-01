cf204e7c972e24fba2ec44e553ad2ce3a99d0dca
MoonSungBin95 <66113141+MoonSungBin95@users.noreply.github.com>
2024-04-01 10:09:54 +0900

    init commit

---
+++ src/test/java/Movie/MovieprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package Movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
